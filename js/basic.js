9a24d501613d92eaee9ed085237d61db5fd15c4f
Shape Lee <shape1026@gmail.com>
2022-07-12 23:40:31 +0900

    [220712] img,js 정리 및 movie page 추가

---
+++ js/basic.js
@@ -0,0 +1,79 @@
+'use strict';
+/*********** go to top ***********/
+const goToTop = document.getElementById("goToTop");
+
+const scrollCheck = () => {
+  let pageYOffset = window.pageYOffset;
+
+  if(pageYOffset !== 0) {
+    goToTop.classList.add('show');
+  } else {
+    goToTop.classList.remove('show');
+  }
+}
+
+const moveTop = () => {
+  if(window.pageYOffset > 0) {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+}
+
+window.addEventListener("scroll",scrollCheck);
+goToTop.addEventListener("click", moveTop);
+
+
+/*********** header up down ***********/
+const header = document.getElementById("header");
+
+let beforeScroll = 0;
+const scrollUpDown = () => {
+  header.className = "";
+  
+  if(253 < window.pageYOffset){
+    header.className = "header-down"
+    
+    if(beforeScroll > window.pageYOffset) {
+      header.className = "header-up"
+    }
+
+  }
+	beforeScroll = window.pageYOffset;
+};
+
+const createLine = () => {
+  if(500 < window.pageYOffset) {
+    header.classList.add("header-line");
+  } else {
+    header.classList.remove("header-line");
+  }
+}
+
+window.addEventListener("scroll", scrollUpDown);
+window.addEventListener("scroll", createLine);
+
+/*********** mobile menu ***********/
+const mobileMenu = document.getElementById("m_gnb");
+const viewMenu = document.querySelector(".menu_icon");
+const closeBtn = document.querySelector(".x_btn");
+
+const addView = () => mobileMenu.classList.add("m_gnb-view");
+const closeView = () => mobileMenu.classList.remove("m_gnb-view");
+
+
+viewMenu.addEventListener("click", addView);
+closeBtn.addEventListener("click", closeView);
+
+/*********** serch ***********/
+const serchContainer = document.getElementById("serch");
+const serchBtn = document.querySelector(".serch_icon");
+const serchXBtn = serchContainer.querySelector(".x_btn");
+
+const viewForm = () => {
+  serchContainer.classList.toggle("serch-view");
+}
+
+serchBtn.addEventListener("click", viewForm);
+serchXBtn.addEventListener("click", viewForm)
--- js/main.js
+++ js/main.js
@@ -1,68 +1,3 @@
-'use strict';
-/*********** go to top ***********/
-const goToTop = document.getElementById("goToTop");
-
-const scrollCheck = () => {
-  let pageYOffset = window.pageYOffset;
-
-  if(pageYOffset !== 0) {
-    goToTop.classList.add('show');
-  } else {
-    goToTop.classList.remove('show');
-  }
-}
-
-const moveTop = () => {
-  if(window.pageYOffset > 0) {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-}
-
-window.addEventListener("scroll",scrollCheck);
-goToTop.addEventListener("click",moveTop);
-
-
-/*********** header up down ***********/
-const header = document.getElementById("header");
-
-let beforeScroll = 0;
-const scrollUpDown = () => {
-  header.className = "";
-  
-  if(253 < window.pageYOffset){
-    header.className = "header-down"
-    
-    if(beforeScroll > window.pageYOffset) {
-      header.className = "header-up"
-    }
-
-  }
-	beforeScroll = window.pageYOffset;
-
-  if(500 < window.pageYOffset) {
-    header.classList.add("header-line");
-  } else {
-    header.classList.remove("header-line");
-  }
-};
-
-window.addEventListener("scroll",scrollUpDown);
-
-
-/*********** mobile menu ***********/
-const mobileMenu = document.getElementById("m_gnb");
-const viewMenu = document.querySelector(".menu_icon");
-const closeBtn = document.querySelector(".x_btn");
-
-const addView = () => mobileMenu.classList.add("m_gnb-view");
-const closeView = () => mobileMenu.classList.remove("m_gnb-view");
-
-
-viewMenu.addEventListener("click", addView);
-closeBtn.addEventListener("click", closeView);
 
 /*********** like heart ***********/
 const likeAll = document.querySelectorAll(".like");
@@ -73,36 +8,28 @@ for(let i = 0; i < likeAll.length; i++) {
   })
 }
 
-
 /*********** preview tab ***********/
 const tabMenu = document.querySelector(".preview-tab");
 const tabButtonLi = tabMenu.getElementsByTagName("li");
 const tabContent = document.querySelectorAll(".preview-content");
 
-const activeTab = () => {
+const activeTab = (e) => {
+  const selectTab = e.currentTarget;
   for(let i = 0; i < tabButtonLi.length; i++) {
-    tabButtonLi[i].classList.toggle("active")
+    tabButtonLi[i].classList.remove("active")
   }
+  
+  selectTab.classList.add("active");
 
-  for(let i = 0; i < tabContent.length; i++){
-    tabContent[i].classList.toggle("active");
+  if(tabButtonLi[0].className === 'active') {
+    tabContent[0].classList.add("active");
+    tabContent[1].classList.remove("active");
+  } else {
+    tabContent[1].classList.add("active");
+    tabContent[0].classList.remove("active");
   }
-
 }
 
 for(let i = 0; i < tabButtonLi.length; i++) {
   tabButtonLi[i].addEventListener("click", activeTab)
 }
-
-
-/*********** serch ***********/
-const serchContainer = document.getElementById("serch");
-const serchBtn = document.querySelector(".serch_icon");
-const serchXBtn = serchContainer.querySelector(".x_btn");
-
-const viewForm = () => {
-  serchContainer.classList.toggle("serch-view");
-}
-
-serchBtn.addEventListener("click", viewForm);
-serchXBtn.addEventListener("click", viewForm)
